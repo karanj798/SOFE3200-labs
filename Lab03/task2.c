d53068a1c3c2bdf2c30772f4d1345122ce23cd75
Karan <karanj798@gmail.com>
2018-11-02 19:53:51 -0400

    Implemented IPC

--- Lab03/task2.c
+++ Lab03/task2.c
@@ -4,44 +4,34 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
-char *replace(char*str, char*orig, char*rep)
+char *replace(char *str, char *orig, char *rep)
 {
-   char *tempBuf = 0; //unknown length
-   tempBuf = malloc(strlen(str)); //set to current length of buffer
+    char *tempBuf = 0;             //unknown length
+    tempBuf = malloc(strlen(str)); //set to current length of buffer
 
+    int runSentinal = 1; // run until all instances of word has been found
+    while (runSentinal == 1)
+    {
 
-   int runSentinal = 1; // run until all instances of word has been found
-   while (runSentinal == 1) {
+        char *p = strstr(str, orig); //Find end of first occurance
+        if (p == NULL)               //If DNE, then don't continue running
+        {
+            runSentinal = 0;
+            return str;
+        }
+        int diff = strlen(rep) - strlen(orig); //Find out if the target char is larger/smaller
 
-      char *p = strstr(str, orig); //Find end of first occurance
-      if (p == NULL) //If DNE, then don't continue running
-      {
-         runSentinal = 0; 
-         return str;
-      }
-      int diff = strlen(rep) - strlen(orig); //Find out if the target char is larger/smaller
+        tempBuf = malloc(strlen(str) + diff - 1); //change size of buffer accordingly
+        int index = p - str;
+        strncpy(tempBuf, str, index); // Copy characters from str up to the start of
+        sprintf(tempBuf + index, "%s%s", rep, p + strlen(orig));
 
+        strcpy(str, tempBuf); //copy buffer to original string
+    }
 
-      
-
-
-      tempBuf = malloc(strlen(str) + diff);//change size of buffer accordingly
-      int index = p - str;
-      strncpy(tempBuf, str, index); // Copy characters from str up to the start of
-    
-      sprintf(tempBuf + index, "%s%s", rep, p + strlen(orig));
-      //printf("NEW REPLACEMENT\n");
-      //printf("%s\n", tempBuf);
-
-
-      strcpy(str, tempBuf); //copy buffer to original string
-
-
-      
-   }
-
-   return tempBuf;
+    return tempBuf;
 }
 
 int main()
@@ -49,7 +39,7 @@ int main()
 
     // Declaring file descriptors
     int fd, fd1;
-    char* buffer = 0;
+    char *buffer = 0;
 
     // Using stat function to get size of the files (bytes = characters)
     struct stat st;
@@ -58,10 +48,10 @@ int main()
 
     // Initiating character array to read the file.
 
-    buffer = malloc(bytesRead);
+    buffer = malloc(bytesRead - 1);
     // Handling file I/O
     fd = open("Input.txt", O_RDONLY);
-    fd1 = open("Dante.txt", O_WRONLY | O_CREAT, 0600);
+    fd1 = open("Dante.txt", O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
     // Error in reading Input.txt file
     if (fd == -1)
@@ -69,22 +59,20 @@ int main()
         perror("Unable to open Input.txt");
         return -1;
     }
+    clock_t start = clock();
     read(fd, buffer, bytesRead - 1); //read file into buffer
 
-
-    replace(buffer, "Inferno", "Paradisioooooooooooooooooooooooooooooooooooooooo");
-    replace(buffer, "In dark woods", "using Windows 8.111111111111111111111");
-    replace(buffer, "those woods", "Windows 8.11111111111111111111111");
+    replace(buffer, "Inferno", "Paradisio");
+    replace(buffer, "In dark woods", "using Windows 8.1");
+    replace(buffer, "those woods", "Windows 8.1");
     replace(buffer, "to enter", "to use 8.1");
     replace(buffer, "crest", "screen");
     replace(buffer, "Below a hill", "Before a monitor");
     replace(buffer, "shoulders", "GUI");
     replace(buffer, "planet", "UNIX");
 
-
     write(fd1, buffer, strlen(buffer));
-
-
-    
-
+    clock_t end = clock();
+    float sec = (float)(end - start) / CLOCKS_PER_SEC;
+    printf("Time elapsed: %fs ", sec);
 }
--- Lab03/task3.c
+++ Lab03/task3.c
@@ -3,31 +3,74 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+char *replace(char *str, char *orig, char *rep)
+{
+    char *tempBuf = 0;             //unknown length
+    tempBuf = malloc(strlen(str)); //set to current length of buffer
+
+    int runSentinal = 1; // run until all instances of word has been found
+    while (runSentinal == 1)
+    {
+        char *p = strstr(str, orig); //Find end of first occurance
+        if (p == NULL)               //If DNE, then don't continue running
+        {
+            runSentinal = 0;
+            return str;
+        }
+        int diff = strlen(rep) - strlen(orig); //Find out if the target char is larger/smaller
+
+        tempBuf = malloc(strlen(str) + diff - 1); //change size of buffer accordingly
+        int index = p - str;
+        strncpy(tempBuf, str, index); // Copy characters from str up to the start of
+        sprintf(tempBuf + index, "%s%s", rep, p + strlen(orig));
+        strcpy(str, tempBuf); //copy buffer to original string
+    }
+    return tempBuf;
+}
 
 int main()
 {
     int bytesRead;
-    int fd[2]; //fd[0] = read, fd[1] = write
-
+    int fd[2], fd1, fd2; //fd[0] = read, fd[1] = write
+    fd1 = open("AliceInWonderland.txt", O_RDONLY);
+    fd2 = open("Dante.txt", O_WRONLY | O_CREAT | O_TRUNC, 0600);
     pipe(fd);
 
     struct stat st;
     stat("AliceInWonderland.txt", &st);
     int bytes = st.st_size;
-    char buffer[bytes];
+    char parentBuffer[bytes];
+    char childBuffer[bytes];
+    clock_t start = clock();
+
     if (fork() != 0) // parent
     {
-        close(fd[1]);
-        fd[0] = open("AliceInWonderland.txt", O_RDONLY);
-        bytesRead = read(fd[0], buffer, bytes);
-        printf("%s", buffer); // Printing Alice.txt
-        close(fd[0]);
+        close(fd[0]); // Close read end
+        bytesRead = read(fd1, parentBuffer, bytes);
+        write(fd[1], parentBuffer, bytesRead); //Writing to pipe
+        close(fd[1]);                          // Close write end
     }
     else
-    {
+    {                 // child
+        close(fd[1]); // Close write end
+        read(fd[0], childBuffer, sizeof(childBuffer));
+        close(fd[0]); // Close read end
+        replace(childBuffer, "Inferno", "Paradisio");
+        replace(childBuffer, "In dark woods", "using Windows 8.1");
+        replace(childBuffer, "those woods", "Windows 8.1");
+        replace(childBuffer, "to enter", "to use 8.1");
+        replace(childBuffer, "crest", "screen");
+        replace(childBuffer, "Below a hill", "Before a monitor");
+        replace(childBuffer, "shoulders", "GUI");
+        replace(childBuffer, "planet", "UNIX");
+
+        write(fd2, childBuffer, sizeof(childBuffer));
         close(fd[0]);
-        fd[1] = open("AliceInWonderland_mod.txt", O_WRONLY | O_CREAT, 0600);
-        write(fd[1], "Hello World", strlen("Hello World"));
-        close(fd[1]);
     }
+    clock_t end = clock();
+    float sec = (float)(end - start) / CLOCKS_PER_SEC;
+    printf("Time elapsed: %fs ", sec);
 }
